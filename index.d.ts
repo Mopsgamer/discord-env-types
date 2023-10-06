48d9944b985002288beaf02ab022a3cb04bd6ac1
Lev <79159094+Mopsgamer@users.noreply.github.com>
2023-10-06 13:41:21 +0200

    add
    $
    __SENTRY_IPC__
    __timingFunction
    __SECRET_EMOTION__
    __sentry_instrumentation_handlers__
    _ws
    GLOBAL_ENV

--- index.d.ts
+++ index.d.ts
@@ -1,41 +1,60 @@
 export * from "./lib";
-import type {
-  DiscordErrors as TDiscordErrors,
-  DiscordNative as TDiscordNative,
-  DiscordSentry as TDiscordSentry,
-  DiscordVideo as TDiscordVideo,
+import {
+  __BILLING_STANDALONE__,
   __DISCORD_WINDOW_ID,
   __localeData__,
-  __BILLING_STANDALONE__,
   __OVERLAY__,
+  __SECRET_EMOTION__,
   __SENTRY__,
+  __sentry_instrumentation_handlers__,
+  __SENTRY_IPC__,
+  _ws,
+  DiscordErrors,
+  DiscordNative,
+  DiscordSentry,
+  DiscordVideo,
+  GLOBAL_ENV,
   webpackChunkdiscord_app,
 } from "./lib";
-import type lodash from "lodash";
+import * as lodash from "lodash";
 
 declare global {
-  const _: lodash.__;
+  const __BILLING_STANDALONE__: __BILLING_STANDALONE__;
   const __DISCORD_WINDOW_ID: __DISCORD_WINDOW_ID;
   const __localeData__: __localeData__;
-  const __BILLING_STANDALONE__: __BILLING_STANDALONE__;
   const __OVERLAY__: __OVERLAY__;
+  const __SECRET_EMOTION__: __SECRET_EMOTION__;
   const __SENTRY__: __SENTRY__;
+  const __sentry_instrumentation_handlers__: __sentry_instrumentation_handlers__;
+  const __SENTRY_IPC__: __SENTRY_IPC__;
+  const __timingFunction: () => number;
+  const _: lodash.__;
+  const _ws: _ws;
+  const $: typeof document.querySelector;
+  const DiscordErrors: DiscordErrors;
+  const DiscordNative: DiscordNative | undefined;
+  const DiscordSentry: DiscordSentry;
+  const DiscordVideo: DiscordVideo;
+  const GLOBAL_ENV: GLOBAL_ENV;
   const webpackChunkdiscord_app: webpackChunkdiscord_app;
-  const DiscordErrors: TDiscordErrors;
-  const DiscordNative: TDiscordNative | undefined;
-  const DiscordSentry: TDiscordSentry;
-  const DiscordVideo: TDiscordVideo;
   interface Window {
-    _: typeof _;
+    __BILLING_STANDALONE__: typeof __BILLING_STANDALONE__;
     __DISCORD_WINDOW_ID: typeof __DISCORD_WINDOW_ID;
     __localeData__: typeof __localeData__;
-    __BILLING_STANDALONE__: typeof __BILLING_STANDALONE__;
     __OVERLAY__: typeof __OVERLAY__;
+    __SECRET_EMOTION__: typeof __SECRET_EMOTION__;
     __SENTRY__: typeof __SENTRY__;
-    webpackChunkdiscord_app: typeof webpackChunkdiscord_app;
+    __sentry_instrumentation_handlers__: typeof __sentry_instrumentation_handlers__;
+    __SENTRY_IPC__: typeof __SENTRY_IPC__;
+    __timingFunction: typeof __timingFunction;
+    _: typeof _;
+    _ws: typeof _ws;
+    $: typeof $;
     DiscordErrors: typeof DiscordErrors;
     DiscordNative: typeof DiscordNative;
     DiscordSentry: typeof DiscordSentry;
     DiscordVideo: typeof DiscordVideo;
+    GLOBAL_ENV: typeof GLOBAL_ENV;
+    webpackChunkdiscord_app: typeof webpackChunkdiscord_app;
   }
 }
--- lib/other.d.ts
+++ lib/other.d.ts
@@ -1,11 +1,31 @@
+import classNames from "classnames";
 import { SentryHub } from "./Sentry";
-import { DiscordLocaleData, DiscordLocaleKey, UnknownMethod } from "./custom";
+import {
+  DiscordLocaleData,
+  DiscordLocaleKey,
+  ReleaseChannel,
+  UnknownMethod,
+} from "./custom";
+import emotion from "@emotion/react";
 
 export type __DISCORD_WINDOW_ID = string;
 export type __isReactDndBackendSetUp = boolean;
 export type __localeData__ = Record<DiscordLocaleKey, DiscordLocaleData>;
 export type __BILLING_STANDALONE__ = boolean;
 export type __OVERLAY__ = boolean;
+export type _ws = null | unknown;
+export type __sentry_instrumentation_handlers__ = Record<
+  "keypress",
+  {
+    refCount: number;
+    handler(e: Element): void;
+  }
+>;
+export interface __SENTRY_IPC__ {
+  sendEnvelope: UnknownMethod<void>;
+  sendEvent: UnknownMethod<void>;
+  sendScope: UnknownMethod<void>;
+}
 export interface __SENTRY__ {
   globalEventProcessors: UnknownMethod[];
   hub: SentryHub;
@@ -20,6 +40,64 @@ export interface __SENTRY__ {
     trace: UnknownMethod<void>;
     warn: UnknownMethod<void>;
   };
+}
+export interface __SECRET_EMOTION__ {
+  caches: Pick<
+    emotion.EmotionCache,
+    "registered" | "inserted" | "key" | "nonce"
+  >;
+  sheet: {
+    ctr: number;
+    injected: boolean;
+    isSpeedy: boolean;
+    opts: unknown;
+    tags: [HTMLStyleElement];
+    flush: emotion.EmotionCache["sheet"]["flush"];
+    inject(): void;
+    insert: emotion.EmotionCache["sheet"]["insert"];
+    speedy(setSpeedy: boolean): void;
+  };
+  injectGlobal: UnknownMethod<void>;
+  css: typeof emotion.css;
+  cx: typeof classNames;
+  flush(): void;
+  keyframes: typeof emotion.keyframes;
+  getRegisteredStyles(registeredStyles: string[], className: string): string;
+  merge(className: string, source: Record<string, unknown>): string;
+  hydrate(ids: unknown[]): void;
+}
+export interface GLOBAL_ENV {
+  ACTIVITY_APPLICATION_HOST: string;
+  ADYEN_KEY: string;
+  ALGOLIA_KEY: string;
+  API_ENDPOINT: string;
+  API_VERSION: number;
+  ASSET_ENDPOINT: string;
+  BRAINTREE_KEY: string;
+  CDN_HOST: string;
+  DEVELOPERS_ENDPOINT: string;
+  GATEWAY_ENDPOINT: string;
+  GIFT_CODE_HOST: string;
+  GUILD_TEMPLATE_HOST: string;
+  HTML_TIMESTAMP: number;
+  INVITE_HOST: string;
+  MARKETING_ENDPOINT: string;
+  MEDIA_PROXY_ENDPOINT: string;
+  MIGRATION_DESTINATION_ORIGIN: string;
+  MIGRATION_SOURCE_ORIGIN: string;
+  NETWORKING_ENDPOINT: string;
+  PROJECT_ENV: string;
+  PUBLIC_PATH: string;
+  RELEASE_CHANNEL: ReleaseChannel;
+  REMOTE_AUTH_ENDPOINT: string;
+  RTC_LATENCY_ENDPOINT: string;
+  SENTRY_TAGS: {
+    buildId: string;
+    buildType: string;
+  };
+  STRIPE_KEY: string;
+  WEBAPP_ENDPOINT: string;
+  WIDGET_ENDPOINT: string;
 }
 
 export type webpackChunkdiscord_app = [number[], Record<number, unknown>][];
